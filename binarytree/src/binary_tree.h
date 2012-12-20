8de14336315c8015b12461850c3417880f64ae03
Steven Dickinson <steven@orangestems.co.uk>
2012-12-20 12:24:40 +0000

    Add entries to tree, and check if they are present

--- binarytree/src/binary_tree.h
+++ binarytree/src/binary_tree.h
@@ -7,19 +7,68 @@ template<typename T>
 class BinaryTree {
 private:
     struct binary_tree_node_t {
-        T *m_left;
-        T *m_right;
+        T m_value;
 
         binary_tree_node_t *m_leftptr;
         binary_tree_node_t *m_rightptr;
     };
 
-    binary_tree_node_t head;
+    binary_tree_node_t *m_head;
+
+    int m_node_count;
 
 public:
     BinaryTree() {
-        this->head.m_leftptr = this->head.m_rightptr = 0;
-        this->head.m_right = this->head.m_left = 0;
+        this->m_head = 0;
+        this->m_node_count = 0;
+    }
+
+    void add(T value) {
+        binary_tree_node_t *prev = NULL;
+        binary_tree_node_t *curr = this->m_head;
+        bool left = true;
+
+        while(true) {
+            if (!curr) {
+                curr = new binary_tree_node_t();
+                curr->m_leftptr = curr->m_rightptr = 0;
+                curr->m_value = value;
+                this->m_node_count++;
+
+                if (prev) {
+                    if (left) {
+                        prev->m_leftptr = curr;
+                    } else {
+                        prev->m_rightptr = curr;
+                    }
+                } else {
+                    this->m_head = curr;
+                }
+                break;
+            } else {
+                if (curr->m_value == value) break;
+
+                prev = curr;
+                left = value <= curr->m_value;
+                curr = left ? curr->m_leftptr : curr->m_rightptr;
+            }
+        }
+    }
+
+    int getNodeCount() {
+        return this->m_node_count;
+    }
+
+    bool isValuePresent(T value) {
+        binary_tree_node_t *curr = this->m_head;
+
+        while(curr) {
+            if (curr->m_value == value) return true;
+            if (value < curr->m_value) curr = curr->m_leftptr;
+            else curr = curr->m_rightptr;
+        }
+
+        return false;
     }
 
 };
@@ -28,3 +77,4 @@ public:
 
 
 #endif
+
--- binarytree/src/test/binary_tree_test.h
+++ binarytree/src/test/binary_tree_test.h
@@ -8,13 +8,53 @@ class BinaryTreeTest: public CPPUNIT_NS::TestFixture {
 public:
 
     void testConstruct() {
-        CPPUNIT_ASSERT ( new sd::BinaryTree<int>() != 0);
+        CPPUNIT_ASSERT ( new BinaryTree<int>() != 0);
+    }
+
+    void testCountIncreasesWhenValueAdded() {
+        BinaryTree<int> tree;
+
+        tree.add(1);
+
+        CPPUNIT_ASSERT_EQUAL(1, tree.getNodeCount());
+
+        tree.add(2);
+        tree.add(5);
+        
+        CPPUNIT_ASSERT_EQUAL(3, tree.getNodeCount());
+    }
+
+    void testIsValuePresent() {
+        BinaryTree<int> tree;
+
+        tree.add(5);
+        tree.add(6);
+        tree.add(58);
+        tree.add(12);
+        tree.add(30);
+        tree.add(32);
+        tree.add(16);
+
+        CPPUNIT_ASSERT(tree.isValuePresent(5));
+        CPPUNIT_ASSERT(tree.isValuePresent(6));
+        CPPUNIT_ASSERT(tree.isValuePresent(58));
+        CPPUNIT_ASSERT(tree.isValuePresent(12));
+        CPPUNIT_ASSERT(tree.isValuePresent(30));
+        CPPUNIT_ASSERT(tree.isValuePresent(32));
+
+
+        CPPUNIT_ASSERT(!tree.isValuePresent(29));
+        CPPUNIT_ASSERT(!tree.isValuePresent(4));
     }
 
     CPPUNIT_TEST_SUITE(BinaryTreeTest);
     CPPUNIT_TEST( testConstruct );
+    CPPUNIT_TEST( testCountIncreasesWhenValueAdded );
+    CPPUNIT_TEST (testIsValuePresent );
     CPPUNIT_TEST_SUITE_END();
 
 };
 
 };
+
+
